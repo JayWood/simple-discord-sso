083cc7a8a5971936dc14e9e953b8c8c970bfde6c
JayWood <jjwood2004@gmail.com>
2022-01-10 22:01:59 -0500

    Create a component for re-use

---
+++ src/assets/frontend/LoginButton/LoginButton.js
@@ -0,0 +1,22 @@
+import {styles} from "./style";
+import {css, cx} from "@emotion/css";
+
+const LoginButton = ({button}) => (
+    <>
+        <hr className={styles.separator} />
+        <a
+            href={button.discordAuthLink}
+            className={ cx( styles.button, css`
+                    background-color: ${styles.discordColors[button.bgColor]};
+               ` )}
+        >
+            <img
+                src={button.logoBaseUrl + "small_logo_" + button.logoColor + "_RGB.svg" }
+                alt={button.altText}
+                className={styles.logo}
+            />
+        </a>
+    </>
+);
+
+export default LoginButton;
