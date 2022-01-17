803d924f0fc6b47d1f66f6f7ef4331a55b8446d7
JayWood <jjwood2004@gmail.com>
2022-01-16 19:14:36 -0500

    Formatting

--- src/assets/frontend/LoginButton/LoginButton.js
+++ src/assets/frontend/LoginButton/LoginButton.js
@@ -1,22 +1,30 @@
-import {styles} from "./style";
-import {css, cx} from "@emotion/css";
+import { styles } from './style';
+import { css, cx } from '@emotion/css';
 
-const LoginButton = ({button}) => (
-    <>
-        <hr className={styles.separator} />
-        <a
-            href={button.discordAuthLink}
-            className={ cx( styles.button, css`
-                    background-color: ${styles.discordColors[button.bgColor]};
-               ` )}
-        >
-            <img
-                src={button.logoBaseUrl + "small_logo_" + button.logoColor + "_RGB.svg" }
-                alt={button.altText}
-                className={styles.logo}
-            />
-        </a>
-    </>
+const LoginButton = ({ button }) => (
+	<>
+		<hr className={styles.separator} />
+		<a
+			href={button.discordAuthLink}
+			className={cx(
+				styles.button,
+				css`
+					background-color: ${styles.discordColors[button.bgColor]};
+				`
+			)}
+		>
+			<img
+				src={
+					button.logoBaseUrl +
+					'small_logo_' +
+					button.logoColor +
+					'_RGB.svg'
+				}
+				alt={button.altText}
+				className={styles.logo}
+			/>
+		</a>
+	</>
 );
 
 export default LoginButton;
--- src/assets/frontend/LoginButton/index.js
+++ src/assets/frontend/LoginButton/index.js
@@ -1,20 +1,23 @@
-import domReady from "@wordpress/dom-ready";
-import {css, cx} from "@emotion/css";
-import {render} from "@wordpress/element";
-import {styles} from "./style.js";
-import LoginButton from "./LoginButton";
+import domReady from '@wordpress/dom-ready';
+import { css, cx } from '@emotion/css';
+import { render } from '@wordpress/element';
+import { styles } from './style.js';
+import LoginButton from './LoginButton';
 
-domReady( () => {
-   const formElement = document.getElementById( 'loginform' );
-   if ( ! formElement ) {
-       return;
-   }
+domReady(() => {
+	const formElement = document.getElementById('loginform');
+	if (!formElement) {
+		return;
+	}
 
-   const discordSettings = window?.jwDiscord || {};
-   const node = document.createElement( 'div' );
-   node.setAttribute( 'id', 'simple-discord-sso-login' );
-   node.setAttribute( 'class', styles.wrapper );
-   formElement.appendChild( node );
+	const discordSettings = window?.jwDiscord || {};
+	const node = document.createElement('div');
+	node.setAttribute('id', 'simple-discord-sso-login');
+	node.setAttribute('class', styles.wrapper);
+	formElement.appendChild(node);
 
-   render( <LoginButton {...discordSettings} />, document.getElementById( 'simple-discord-sso-login' ) );
-} );
+	render(
+		<LoginButton {...discordSettings} />,
+		document.getElementById('simple-discord-sso-login')
+	);
+});
