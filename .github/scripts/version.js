2a59ec640c231547bf000b1256f38c7b6974580b
JayWood <jjwood2004@gmail.com>
2022-01-23 12:45:18 -0500

    Successfully replace in .txt and .md file

--- .github/scripts/version.js
+++ .github/scripts/version.js
@@ -1,20 +1,29 @@
 const {version} = require( '../../package.json' );
 const fs = require( 'fs' );
 
-const files = {
-  "readme.txt": "pattern"
-};
+// Replace in .txt file
+fs.readFile( "readme.txt", 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
 
-for( const file in files ) {
-  fs.readFile( file, 'utf8', function (err,data) {
-    if (err) {
-      return console.log(err);
-    }
-    var result = data.replace(/Stable tag: ([0-9\.]+)/g, 'Stable tag: ' + version );
-
-    fs.writeFile( file, result, 'utf8', function (err) {
-      if (err) return console.log(err);
-    });
+  const result = data.replace(/Stable tag: ([0-9\.]+)/g, 'Stable tag: ' + version );
+  fs.writeFile( "readme.txt", result, 'utf8', function (err) {
+    if (err) console.log( err ) && process.exit( 1 );
   });
-}
+});
+
+// Replace in md file
+fs.readFile( "readme.md", 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+
+  // Has issues with double asterisks so sticking with \W{2} instead
+  const result = data.replace(/Stable tag:\W{2} ([0-9\.])+/gi, 'Stable tag:** ' + version );
+  console.log( result );
+  fs.writeFile( "readme.md", result, 'utf8', function (err) {
+    if (err) console.log(err) && process.exit( 1 );
+  });
+});
 
